1ad86dce37bb8fa20561bc42c703a0447ed63673
svadlama <srinivas.satish@yahoo.com>
2020-04-21 21:13:13 -0400

    test from mac3

--- testscript.sh
+++ testscript.sh
@@ -1,4 +1,4 @@
 echo "This script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "This- is completed from terminel test8--"
+echo "This- is completed from terminel test101010101010101010108--"
